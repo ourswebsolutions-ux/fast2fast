52acd46dc8fc7c717bfe7d008edf303b300459ee
Ali Hamza <alihamza@meezansoftwarehouse.com>
2026-04-10 23:20:39 +0500

    inital project

---
+++ src/app/home/page.tsx
@@ -0,0 +1,13 @@
+import GridLayout from "@/components/layouts/GridLayout";
+// import GridLayou
+export default function Page() {
+  return (
+    <GridLayout>
+        <div className="main bg-white lg:col-span-12">
+  {/* <aside className="lg:col-span-3">Left Menu</aside>
+  <section className="lg:col-span-6">Hero</section>
+  <aside className="lg:col-span-3">Right Banners</aside> */}
+  </div>
+</GridLayout>
+  );
+}
---
+++ src/components/layouts/GridLayout.tsx
@@ -0,0 +1,24 @@
+// src/components/layouts/GridLayout.tsx
+
+import type { ReactNode } from "react";
+import { cn } from "@/lib/utils";
+
+interface GridLayoutProps {
+  children: ReactNode;
+  className?: string;
+}
+
+export default function GridLayout({ children, className }: GridLayoutProps) {
+  return (
+    <main className="w-full px-4 sm:px-6 lg:px-8">
+      <div
+        className={cn(
+          "mx-auto grid min-h-screen w-full max-w-7xl grid-cols-1 gap-4 sm:grid-cols-4 lg:grid-cols-12",
+          className
+        )}
+      >
+        {children}
+      </div>
+    </main>
+  );
+}
